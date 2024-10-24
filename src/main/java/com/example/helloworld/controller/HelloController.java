78071b16b03f5251cc3e08c7c889b52b4b56adb5
sandya kandregula <52678059+sandy2341@users.noreply.github.com>
2024-10-24 12:42:55 +0530

    Update HelloController.java

--- src/main/java/com/example/helloworld/controller/HelloController.java
+++ src/main/java/com/example/helloworld/controller/HelloController.java
@@ -8,6 +8,6 @@ public class HelloController {
 
     @GetMapping("/")
     public String hello() {
-        return "Hello Team, This DevOps Training. Presenter - Sandeep Siyadri";
+        return "maven application successfully running";
     }
 }
